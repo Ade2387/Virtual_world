82b59211d03f3c16d2b1633d46ead54e2b804c86
Arno Debelle <debelle.a@gmail.com>
2024-03-08 17:58:19 +0100

    lecture 4 done

--- js/world.js
+++ js/world.js
@@ -5,6 +5,7 @@ class World {
     this.roadWidth = roadWidth;
 
     this.envelopes = [];
+    this.roadBoarders = [];
 
     this.generate();
   }
@@ -16,11 +17,19 @@ class World {
         new Envelope(seg, this.roadWidth, this.roadRoundness)
       );
     }
+
+    this.roadBoarders = Polygon.union(this.envelopes.map((e) => e.poly));
   }
 
   draw(ctx) {
     for (const env of this.envelopes) {
-      env.draw(ctx);
+      env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15});
+    }
+    for (const seg of this.graph.segments) {
+      seg.draw(ctx, { color: "white", width: 4, dash: [10, 10]});
+    }
+    for (const seg of this.roadBoarders) {
+      seg.draw(ctx, { color: "white", width: 4});
     }
   }
 }
